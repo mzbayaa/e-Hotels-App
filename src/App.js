5b6aa52c07ffe299a4a66cee4cb5d11305458ca6
Mohamad Bayaa <mbaya053@uottawa.ca>
2024-03-19 16:35:58 -0400

    set up components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+// import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./Components/Pages/Home";
+import { Hotel } from "./Components/Pages/Hotel";
+import { List } from "./Components/Pages/List";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/hotels" exact element={<List />} />
+        <Route path="/hotels/:id" exact element={<Hotel />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
