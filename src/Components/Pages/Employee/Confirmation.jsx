d6c0b104e26157783b27da78a59086b3f7e55e4e
Mohamad Bayaa <mbaya053@uottawa.ca>
2024-04-01 01:12:05 -0400

    employee confirmation page

---
+++ src/Components/Pages/Employee/Confirmation.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Confirmation.css";
+import { useNavigate } from "react-router-dom";
+
+export default function Confirmation() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="page">
+      <h1 className="header">Transaction Confirmed!</h1>
+      <p className="message">
+        We have succecufully received and confirmed your reservation request.{" "}
+      </p>
+      <p>An email confirming your reservation will be sent to you shortly </p>
+      {/* <p>Booking Number: #{bookingId} </p> */}
+      <p>Confirmation Number: #1111111 </p>
+      <div className="button">
+        <button
+          className="btn btn-success"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Home
+        </button>
+      </div>
+    </div>
+  );
+}
